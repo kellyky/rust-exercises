883d06daeec210ae75231f7dd02f046c5ff9b832
Kelly Popko <kelpopko@gmail.com>
2024-07-18 13:21:38 -0500

    Add parens (leap year) for slighty more efficient check

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -3,7 +3,7 @@ pub fn is_leap_year(year: u64) -> bool {
     let div_by_hundred = if year % 100 == 0 { true } else { false };
     let div_by_four_hundred = if year % 400 == 0 { true } else { false };
 
-    div_by_four && !div_by_hundred || div_by_four_hundred
+    div_by_four && (!div_by_hundred || div_by_four_hundred)
 }
 
 
